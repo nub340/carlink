499b7f8823aa22a1111fc1e4bc511e1e12754cb2
nub340 <nkoier@gmail.com>
2024-09-25 23:50:35 -0700

    commands

--- Arduino IDE/carlink/carlink.ino
+++ Arduino IDE/carlink/carlink.ino
@@ -18,7 +18,7 @@
 
 // Define the CS and INT pins
 #define CAN_CS 15  // Choose a free GPIO for CS
-#define CAN_INT 2
+#define CAN_INT 35
 #define CAN_MOSI 14  // Choose a free GPIO for MOSI
 #define CAN_MISO 19  // Choose a free GPIO for MISO
 #define CAN_SCK 18   // Choose a free GPIO for SCK
@@ -33,6 +33,12 @@
 #define I2C_SDA 21
 #define I2C_SCL 22
 #define SerialAT Serial1
+
+// analog relay pins
+#define WINDOW_UP_RELAY 0
+#define WINDOW_DOWN_RELAY 25
+#define WINDOW_UPDOWN_DELAY 6000
+#define WINDOW_VENT_DELAY 600
 
 #define CERT_BUF_SIZE 2048  // Adjust this size based on your certificate length
 #define PUBLISH_INTERVAL 5000;
@@ -56,6 +62,7 @@ int connectCount = 0;
 unsigned long lastDisplayErrorsTime = 0;
 static volatile unsigned long lastPublishTime = 0;
 unsigned long publishInterval = 5000;
+bool isWindowDown = false;
 
 MCP_CAN CAN(CAN_CS);  // Create CAN object with CS pin
 
@@ -67,7 +74,7 @@ SSLClient client(&base_client);
 PubSubClient mqttClient(client);
 
 // Queue for background task queuing and processesing
-QueueHandle_t mqttQueue;
+//QueueHandle_t mqttQueue;
 
 // Structure to hold raw CAN data for MQTT publishing
 struct CanMessage {
@@ -87,6 +94,7 @@ enum CommandID {
   CMD_LOCK_DOORS,
   CMD_ROLL_WINDOWS_DOWN,
   CMD_ROLL_WINDOWS_UP,
+  CMD_TOGGLE_WINDOW,
   CMD_VENT_WINDOWS,
   CMD_TURN_ON_HVAC,
   CMD_TURN_OFF_HVAC,
@@ -96,7 +104,8 @@ enum CommandID {
   CMD_TILT_SUNROOF,
   CMD_UNTILT_SUNROOF,
   CMD_FRONT_DEFROST,
-  CMD_REAR_DEFROST
+  CMD_REAR_DEFROST,
+  CMD_INVALID  // Special value for invalid commands
 };
 
 struct Command {
@@ -104,36 +113,35 @@ struct Command {
   String params;
 };
 
-void
-setup() {
+void setup() {
   Serial.begin(115200);
   while (!Serial)
     delay(10);
   Serial.flush();
   Serial.println();
-  Serial.println("Begin setup...");
+  Serial.println(F("Begin setup..."));
 
   pinMode(0, OUTPUT);
   digitalWrite(0, LOW);
 
-  Serial.println("\Configuring connectivity...");
+  Serial.println(F("\Configuring connectivity..."));
   if (loadCertificates() && setupModem() && setupAWSIoT() && connectToAWS()) {
-    Serial.println("Connected to AWS. Ready to send and receive...");
+    Serial.println(F("Connected to AWS. Ready to send and receive..."));
   } else {
-    Serial.println("Setup failed!");
+    Serial.println(F("Setup failed!"));
   }
 
   // Initialize MCP2515 CAN controller at 500kbps speed
   SPI.begin(CAN_SCK, CAN_MISO, CAN_MOSI, CAN_CS);
   if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
-    Serial.println("MCP2515 Initialized Successfully!");
+    Serial.println(F("MCP2515 Initialized Successfully!"));
   } else {
-    Serial.println("Error Initializing MCP2515.");
+    Serial.println(F("Error Initializing MCP2515."));
     while (1)
       ;
   }
 
-  mqttQueue = xQueueCreate(100, sizeof(CanMessage));
+  //mqttQueue = xQueueCreate(100, sizeof(std::map<unsigned long, CanMessage>));
 
   // Create a task for publishing to MQTT
   xTaskCreatePinnedToCore(
@@ -145,6 +153,10 @@ setup() {
     NULL,        // Task handle
     0            // Pin task to core 1
   );
+
+  // relay pins
+  pinMode(WINDOW_UP_RELAY, OUTPUT);
+  pinMode(WINDOW_DOWN_RELAY, OUTPUT);
 
   // Enable CAN interrupt
   pinMode(CAN_INT, INPUT);
@@ -194,30 +206,37 @@ void loop() {
 
   // enqueue the latest data for publishing if time interval has elapsed
   if (millis() - lastPublishTime >= publishInterval) {
-    enqueueCanMessages();
+    //enqueueCanMessages();
   }
 
   mqttClient.loop();
 }
 
-void enqueueCanMessages() {
-  // Create a copy of the map or its entries
-  for (const auto& entry : canMessages) {
-    CanMessage message = entry.second;               // Copy message
-    xQueueSend(mqttQueue, &message, portMAX_DELAY);  // Send copy to queue
-  }
-  //canMessages.clear();  // Optionally clear the map if it’s only used for temporary storage
-}
+// void enqueueCanMessages() {
+//   // Create a copy of the map or its entries
+//   // for (const auto& entry : canMessages) {
+//   //   CanMessage message = entry.second;               // Copy message
+//   //   xQueueSend(mqttQueue, &message, portMAX_DELAY);  // Send copy to queue
+//   // }
+//   std::map<unsigned long, CanMessage> canMessagesSnapshot(canMessages);
+//   xQueueSend(mqttQueue, &canMessagesSnapshot, portMAX_DELAY);
+
+//   //canMessages.clear();  // Optionally clear the map if it’s only used for temporary storage
+// }
 
 // Task to handle MQTT publishing
 void MQTTTask(void* pvParameters) {
-  Serial.println("MQTTTask");
-  CanMessage msg;
+  Serial.println(F("MQTTTask"));
+  //std::map<unsigned long, CanMessage> snapshot;
   char payload[100];
 
   while (1) {
     // Wait for new CAN messages in the queue
-    if (xQueueReceive(mqttQueue, &msg, portMAX_DELAY) == pdPASS && millis() - lastPublishTime >= publishInterval) {
+    //if (xQueueReceive(mqttQueue, &snapshot, portMAX_DELAY) == pdPASS && millis() - lastPublishTime >= publishInterval) {
+    if (false && millis() - lastPublishTime >= publishInterval) {
+
+      std::map<unsigned long, CanMessage> snapshot(canMessages);
+      CanMessage msg = snapshot[0x126];
 
       // The motor temperature is at offset 32 bits (4 bytes) and is 16 bits (2 bytes) long
       uint16_t rawTemperature = (msg.data[5] << 8) | msg.data[4];  // Combine two bytes for the temperature value
@@ -232,7 +251,7 @@ void MQTTTask(void* pvParameters) {
       String payload = "{\"motorTemp\":\"" + String(msg_temp) + " °C\",\"millis\":\"" + msg_millis + "\"}";
 
       if (!mqttClient.connected()) {
-        Serial.println("reconnecting to MQTT broker...");
+        Serial.println(F("reconnecting to MQTT broker..."));
         Serial.println(mqttClient.state());
         yield();
         connectToAWS();
@@ -242,13 +261,13 @@ void MQTTTask(void* pvParameters) {
       // Serial.println(ESP.getFreeHeap());
 
       //Publish a test message
-      Serial.print("publishing message: ");
+      Serial.print(F("publishing message: "));
       Serial.println(payload.c_str());
-      if (mqttClient.publish("car/out", payload.c_str())) {
-        Serial.println(" succeeded!");
-      } else {
-        Serial.println(" failed.");
-      }
+      // if (mqttClient.publish("car/out", payload.c_str())) {
+      //   Serial.println(F(" succeeded!"));
+      // } else {
+      //   Serial.println(F(" failed."));
+      // }
       yield();  // Yield CPU control to other tasks
 
       // Serial.print("Free heap after publish: ");
@@ -263,7 +282,7 @@ void MQTTTask(void* pvParameters) {
 
 bool loadCertificates() {
   if (!SPIFFS.begin(true)) {  // 'true' will format the file system if it's corrupted
-    Serial.println("SPIFFS Mount Failed");
+    Serial.println(F("SPIFFS Mount Failed"));
     return false;
     delay(100);
   }
@@ -305,18 +324,18 @@ bool setupModem() {
   digitalWrite(MODEM_PWRKEY, HIGH);
   digitalWrite(MODEM_POWER_ON, HIGH);
 
-  Serial.println("Initializing modem...");
+  Serial.println(F("Initializing modem..."));
   modem.restart();
 
-  Serial.println("Connecting to cellular network...");
+  Serial.println(F("Connecting to cellular network..."));
   while (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
     connectCount++;
-    Serial.print("Failed to connect to cellular network. Fail count: ");
+    Serial.print(F("Failed to connect to cellular network. Fail count: "));
     Serial.println(connectCount);
-    Serial.println("Restarting modem...");
+    Serial.println(F("Restarting modem..."));
     modem.restart();
   }
-  Serial.println("Connected to cellular network");
+  Serial.println(F("Connected to cellular network"));
   return true;
 }
 
@@ -335,14 +354,14 @@ bool setupAWSIoT() {
 }
 
 bool connectToAWS() {
-  Serial.println("Connecting to AWS IoT Core...");
+  Serial.println(F("Connecting to AWS IoT Core..."));
   while (!mqttClient.connected()) {
     if (mqttClient.connect("ESP32_DevModule")) {
-      Serial.print("Connected to AWS IoT Core endpoint: ");
+      Serial.print(F("Connected to AWS IoT Core endpoint: "));
       Serial.println(awsEndpoint);
       mqttClient.subscribe("car/in");
     } else {
-      Serial.print("Failed to connect. Error state=");
+      Serial.print(F("Failed to connect. Error state="));
       Serial.println(mqttClient.state());
       delay(5000);
     }
@@ -352,90 +371,184 @@ bool connectToAWS() {
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+  // Serial.print(F("Message arrived ["));
+  // Serial.print(topic);
+  // Serial.print(F("] "));
+  // for (int i = 0; i < length; i++) {
+  //   Serial.print((char)payload[i]);
+  // }
+  // Serial.println();
+
+  // Deserialize json string into json document
+  String jsonString = String((char*)payload);
+  const size_t capacity = JSON_OBJECT_SIZE(2) + 40;
+  DynamicJsonDocument doc(capacity);
+  DeserializationError error = deserializeJson(doc, jsonString);
+
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
   }
-  Serial.println();
+
+  // Check if "cmd" key exists
+  if (!doc.containsKey("cmd")) {
+    Serial.println(F("Error: 'cmd' key is missing"));
+    return;
+  }
+
+  int cmdValue = doc["cmd"];
+  CommandID commandID = getCommandID(cmdValue);
+
+  // Check if the command is valid
+  if (commandID == CMD_INVALID) {
+    Serial.print(F("Error: Invalid command received: "));
+    Serial.println(cmdValue);
+    return;
+  }
+
+  // Print the enum name
+  Serial.print(F("Command received: "));
+  Serial.println(commandIDToString(commandID));
 
   Command cmd;
-  cmd.id = CMD_ROLL_WINDOWS_DOWN;
-  processRemoteCommand(cmd);
+  cmd.id = commandID;
+
+  // Check if "params" exists and extract it, or set it to an empty string
+  if (doc.containsKey("params")) {
+    cmd.params = String(doc["params"].as<const char*>());  // Params found, assign value
+  } else {
+    cmd.params = "";  // Params not found, assign an empty string
+  }
+
+  processCommand(cmd);
+}
+
+void rollWindowDown(int ms = WINDOW_UPDOWN_DELAY) {
+  Serial.print(F("Rolling window down..."));
+  digitalWrite(WINDOW_UP_RELAY, false);
+  digitalWrite(WINDOW_DOWN_RELAY, true);
+  Serial.println(digitalRead(WINDOW_DOWN_RELAY));
+  delay(ms);
+  digitalWrite(WINDOW_DOWN_RELAY, false);
+  Serial.println(F("done."));
+  Serial.println(digitalRead(WINDOW_DOWN_RELAY));
+}
+
+void rollWindowUp(int ms = WINDOW_UPDOWN_DELAY) {
+  Serial.print(F("Rolling window up..."));
+  digitalWrite(WINDOW_DOWN_RELAY, false);
+  digitalWrite(WINDOW_UP_RELAY, true);
+  Serial.println(digitalRead(WINDOW_UP_RELAY));
+  delay(ms);
+  digitalWrite(WINDOW_UP_RELAY, false);
+  Serial.println(F("done."));
+  Serial.println(digitalRead(WINDOW_UP_RELAY));
+}
+
+void ventWindow(int ms = WINDOW_VENT_DELAY) {
+  Serial.print(F("Venting window..."));
+  rollWindowUp();
+  rollWindowDown(ms);
+}
+
+void processCommand(Command cmd) {
+  switch (cmd.id) {
+    case CMD_ROLL_WINDOWS_DOWN:
+      rollWindowDown();
+      break;
+
+    case CMD_ROLL_WINDOWS_UP:
+      rollWindowUp();
+      break;
+
+    case CMD_TOGGLE_WINDOW:
+      toggleWindow();
+      break;
+
+    case CMD_VENT_WINDOWS:
+      ventWindow();
+      break;
+  }
+}
+
+void toggleWindow() {
+  Serial.print(F("Toggling windw state. Currently: "));
+  Serial.println(isWindowDown);
+  if (isWindowDown) {
+    rollWindowUp();
+  } else {
+    rollWindowDown();
+  }
+  isWindowDown = !isWindowDown;
+}
+
+//= Helpers ==================================================
+// Function to convert an integer to CommandID safely
+CommandID getCommandID(int cmdId) {
+  if (cmdId >= CMD_READ_DATA && cmdId <= CMD_REAR_DEFROST) {
+    return static_cast<CommandID>(cmdId);
+  }
+  return CMD_INVALID;  // Return invalid command if the value is out of range
 }
 
 void displayCANErrors() {
   // Check for errors
   if (CAN.checkError()) {
-    Serial.println("MCP2515 Error Detected!");
-    Serial.print("Errors RX: ");
+    Serial.println(F("MCP2515 Error Detected!"));
+    Serial.print(F("Errors RX: "));
     Serial.print(CAN.errorCountRX());
-    Serial.print(", TX: ");
+    Serial.print(F(", TX: "));
     Serial.println(CAN.errorCountTX());
 
     uint8_t errorFlag = CAN.getError();
     if (errorFlag & MCP_EFLG_RX1OVR) {
-      Serial.println("Receive Buffer 1 Overflow Error");
+      Serial.println(F("Receive Buffer 1 Overflow Error"));
       //CAN.reset();
     }
     if (errorFlag & MCP_EFLG_RX0OVR) {
-      Serial.println("Receive Buffer 0 Overflow Error");
+      Serial.println(F("Receive Buffer 0 Overflow Error"));
       //CAN.reset();
     }
     if (errorFlag & MCP_EFLG_TXBO) {
-      Serial.println("Bus-Off Error");
+      Serial.println(F("Bus-Off Error"));
     }
     if (errorFlag & MCP_EFLG_TXEP) {
-      Serial.println("Transmit Error-Passive");
+      Serial.println(F("Transmit Error-Passive"));
     }
     if (errorFlag & MCP_EFLG_RXEP) {
-      Serial.println("Receive Error-Passive");
+      Serial.println(F("Receive Error-Passive"));
     }
   } else {
     //Serial.println("No Errors Detected.");
   }
 }
 
-void processRemoteCommand(Command cmd) {
-  switch (cmd.id) {
-    case CMD_ROLL_WINDOWS_DOWN:
-      rollWindowDown();
-      break;
-  }
-}
-
-void rollWindowDown() {
-  digitalWrite(0, !digitalRead(0));
-}
-
-//= Helpers ==================================================
-
 void listDir(fs::FS& fs, const char* dirname, uint8_t levels) {
-  Serial.printf("Listing directory: %s\n", dirname);
+  Serial.printf(F("Listing directory: %s\n"), dirname);
 
   File root = fs.open(dirname);
   if (!root) {
-    Serial.println("Failed to open directory");
+    Serial.println(F("Failed to open directory"));
     return;
   }
   if (!root.isDirectory()) {
-    Serial.println("Not a directory");
+    Serial.println(F("Not a directory"));
     return;
   }
 
   File file = root.openNextFile();
   while (file) {
     if (file.isDirectory()) {
-      Serial.print("DIR : ");
+      Serial.print(F("DIR : "));
       Serial.println(file.name());
       if (levels) {
         listDir(fs, file.name(), levels - 1);
       }
     } else {
-      Serial.print("FILE: ");
+      Serial.print(F("FILE: "));
       Serial.print(file.name());
-      Serial.print("  SIZE: ");
+      Serial.print(F("  SIZE: "));
       Serial.println(file.size());
     }
     file = root.openNextFile();
@@ -455,5 +568,27 @@ void readFileToBuffer(const char* path, char* buffer, size_t bufferSize) {
     file.close();
   } else {
     Serial.printf("Failed to open file: %s\n", path);
+  }
+}
+
+const char* commandIDToString(CommandID cmd) {
+  switch (cmd) {
+    case CMD_READ_DATA: return "CMD_READ_DATA";
+    case CMD_UNLOCK_DOORS: return "CMD_UNLOCK_DOORS";
+    case CMD_LOCK_DOORS: return "CMD_LOCK_DOORS";
+    case CMD_ROLL_WINDOWS_DOWN: return "CMD_ROLL_WINDOWS_DOWN";
+    case CMD_ROLL_WINDOWS_UP: return "CMD_ROLL_WINDOWS_UP";
+    case CMD_TOGGLE_WINDOW: return "CMD_TOGGLE_WINDOW";
+    case CMD_VENT_WINDOWS: return "CMD_VENT_WINDOWS";
+    case CMD_TURN_ON_HVAC: return "CMD_TURN_ON_HVAC";
+    case CMD_TURN_OFF_HVAC: return "CMD_TURN_OFF_HVAC";
+    case CMD_HONK: return "CMD_HONK";
+    case CMD_OPEN_SUNROOF: return "CMD_OPEN_SUNROOF";
+    case CMD_CLOSE_SUNROOF: return "CMD_CLOSE_SUNROOF";
+    case CMD_TILT_SUNROOF: return "CMD_TILT_SUNROOF";
+    case CMD_UNTILT_SUNROOF: return "CMD_UNTILT_SUNROOF";
+    case CMD_FRONT_DEFROST: return "CMD_FRONT_DEFROST";
+    case CMD_REAR_DEFROST: return "CMD_REAR_DEFROST";
+    default: return "UNKNOWN_COMMAND";
   }
 }
